eb3aa8f471cd437e203b65bbdfbaaa58488c3bd6
BerdArt <BerdArt@840598.no-reply.drupal.org>
2016-06-09 09:06:25 -0400

    Issue #2149577 by berdyshev: Drupal 8 port for Menu Breadcrumb

---
+++ src/Form/SettingsForm.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace Drupal\menu_breadcrumb\Form;
+
+use Drupal\Component\Utility\SortArray;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class SettingsForm
+ * @package Drupal\menu_breadcrumb\Form
+ */
+class SettingsForm extends ConfigFormBase {
+  /**
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * SettingsForm constructor.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, ModuleHandlerInterface $moduleHandler) {
+    parent::__construct($config_factory);
+    $this->moduleHandler = $moduleHandler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('module_handler')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['menu_breadcrumb.settings'];
+  }
+
+  /**
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   * @return array
+   */
+  public function buildForm(array $form = [], FormStateInterface $form_state) {
+    $config = $this->config('menu_breadcrumb.settings');
+    $form['determine_menu'] = [
+      '#type'          => 'checkbox',
+      '#title'         => t('Use menu the page belongs to for the breadcrumb.'),
+      '#description'   => t('By default, Drupal builds breadcrumb on path basis. If you want to use the menu the active page belongs to for the breadcrumb, enable this option.'),
+      '#default_value' => $config->get('determine_menu'),
+    ];
+
+    $form['disable_admin_page'] = [
+      '#type'          => 'checkbox',
+      '#title'         => t('Disable for admin pages'),
+      '#description'   => t('Do not build menu-based breadcrumbs for admin pages.'),
+      '#default_value' => $config->get('disable_admin_page'),
+    ];
+
+    $form['append_current_page'] = [
+      '#type'          => 'checkbox',
+      '#title'         => t('Append current page to breadcrumb'),
+      '#description'   => t('Choose whether or not the current page should be included in the breadcrumb.'),
+      '#default_value' => $config->get('append_current_page'),
+    ];
+
+    $form['current_page_as_link'] = [
+      '#type'          => 'checkbox',
+      '#title'         => t('Current page as link'),
+      '#description'   => t('Choose whether or not the appended current page title should be a link.'),
+      '#default_value' => $config->get('current_page_as_link'),
+      '#states'        => [
+        'visible' => [
+          ':input[name="append_current_page"]' => ['checked' => TRUE],
+        ],
+      ],
+    ];
+
+    $form['hide_on_single_item'] = [
+      '#type'          => 'checkbox',
+      '#title'         => t('Hide the breadcrumb if the breadcrumb only contains the link to the front page.'),
+      '#description'   => t('Choose whether or not the breadcrumb should be hidden if the breadcrumb only contains a link to the front page (<em>Home</em>.).'),
+      '#default_value' => $config->get('hide_on_single_item'),
+    ];
+
+    $form['remove_home'] = [
+      '#type'          => 'checkbox',
+      '#title'         => t('Remove "Home" link'),
+      '#description'   => t('Removes "Home" link to the front page, in case you already have one.'),
+      '#default_value' => $config->get('remove_home'),
+    ];
+
+    $form['home_as_site_name'] = [
+      '#type'          => 'checkbox',
+      '#title'         => t('Use site name for "Home" link'),
+      '#default_value' => $config->get('home_as_site_name'),
+      '#states'        => [
+        'visible' => [
+          ':input[name="remove_home"]' => ['checked' => FALSE],
+        ],
+      ],
+    ];
+
+    $form['include_exclude'] = [
+      '#type'        => 'fieldset',
+      '#title'       => t('Enable / Disable Menus'),
+      '#description' => t('The breadcrumb will be generated from the first "enabled" menu that contains a menu item for the page. Re-order the list to change the priority of each menu.'),
+    ];
+
+    $form['include_exclude']['note_about_navigation'] = [
+      '#markup' => '<p class="description">' . t("Note: If none of the enabled menus contain an item for a given page, Drupal will look in the 'Navigation' menu by default, even if it is 'disabled' here.") . '</p>',
+    ];
+
+    // Orderable list of menu selections.
+    $form['include_exclude']['menu_breadcrumb_menus'] = [
+      '#type'      => 'table',
+      '#header'    => [t('Menu'), t('Enabled'), t('Weight')],
+      '#empty'     => t('There is no menus yet.'),
+      '#tabledrag' => [
+        [
+          'action'       => 'order',
+          'relationship' => 'sibling',
+          'group'        => 'menus-order-weight',
+        ],
+      ],
+    ];
+
+    foreach ($this->getSortedMenus() as $menu_name => $menu_config) {
+
+      $form['include_exclude']['menu_breadcrumb_menus'][$menu_name] = [
+        '#attributes' => [
+          'class' => ['draggable'],
+        ],
+        '#weight'     => $menu_config['weight'],
+        'title'       => [
+          '#plain_text' => $menu_config['label'],
+        ],
+        'enabled'     => [
+          '#type'          => 'checkbox',
+          '#default_value' => $menu_config['enabled'],
+        ],
+        'weight'      => [
+          '#type'          => 'weight',
+          '#default_value' => $menu_config['weight'],
+          '#attributes'    => ['class' => ['menus-order-weight']],
+        ],
+      ];
+    }
+
+    $form['include_exclude']['description'] = [
+      '#prefix' => '<p class="description">',
+      '#suffix' => '</p>',
+      '#markup' => t('<strong>Default setting</strong> is not a real menu - it defines the default position and enabled status for future menus. If it is "enabled", Menu Breadcrumb will automatically consider newly-added menus when establishing breadcrumbs. If it is disabled, new menus will not be used for breadcrumbs until they have explicitly been enabled here.'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->config('menu_breadcrumb.settings')
+      ->set('determine_menu', (boolean) $form_state->getValue('determine_menu'))
+      ->set('disable_admin_page', (boolean) $form_state->getValue('disable_admin_page'))
+      ->set('append_current_page', (boolean) $form_state->getValue('append_current_page'))
+      ->set('current_page_as_link', (boolean) $form_state->getValue('current_page_as_link'))
+      ->set('hide_on_single_item', (boolean) $form_state->getValue('hide_on_single_item'))
+      ->set('remove_home', (boolean) $form_state->getValue('remove_home'))
+      ->set('home_as_site_name', (boolean) $form_state->getValue('home_as_site_name'))
+      ->set('menu_breadcrumb_menus', $form_state->getValue('menu_breadcrumb_menus'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'menu_breadcrumb_settings';
+  }
+
+  /**
+   * Returns array of menus with properties (enabled, weight, label) sorted by
+   * weight.
+   */
+  protected function getSortedMenus() {
+    $menu_enabled = $this->moduleHandler->moduleExists('menu_ui');
+    $menus = $menu_enabled ? menu_ui_get_menus() : menu_list_system_menus();
+    $menu_breadcrumb_menus = $this->config('menu_breadcrumb.settings')
+      ->get('menu_breadcrumb_menus');
+
+    foreach ($menus as $menu_name => &$menu) {
+      if (!empty($menu_breadcrumb_menus[$menu_name])) {
+        $menu = $menu_breadcrumb_menus[$menu_name] + ['label' => $menu];
+      }
+      else {
+        $menu = ['weight' => 0, 'enabled' => 0, 'label' => $menu];
+      }
+    }
+
+    uasort($menus, function ($a, $b) {
+      return SortArray::sortByWeightElement($a, $b);
+    });
+
+    return $menus;
+  }
+}
---
+++ src/MenuBasedBreadcrumbBuilder.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * @file
+ * MenuBasedBreadcrumbBuilder.php
+ */
+namespace Drupal\menu_breadcrumb;
+
+use Drupal\Component\Utility\SortArray;
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Menu\MenuActiveTrailInterface;
+use Drupal\Core\Menu\MenuLinkManagerInterface;
+use Drupal\Core\Menu\MenuLinkTreeElement;
+use Drupal\Core\Menu\MenuLinkTreeInterface;
+use Drupal\Core\Routing\AdminContext;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Url;
+
+/**
+ * Class MenuBasedBreadcrumbBuilder
+ * @package Drupal\menu_breadcrumb
+ */
+class MenuBasedBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+  /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
+   * @var \Drupal\Core\Menu\MenuActiveTrailInterface
+   */
+  protected $menuActiveTrail;
+
+  /**
+   * @var \Drupal\Core\Menu\MenuLinkManagerInterface
+   */
+  protected $linkManager;
+
+  /**
+   * @var \Drupal\Core\Routing\AdminContext
+   */
+  protected $adminContext;
+
+  public function __construct(ConfigFactoryInterface $configFactory, MenuActiveTrailInterface $menuActiveTrail, MenuLinkManagerInterface $linkManager, AdminContext $adminContext) {
+    $this->configFactory = $configFactory;
+    $this->config = $this->configFactory->get('menu_breadcrumb.settings');
+    $this->menuActiveTrail = $menuActiveTrail;
+    $this->linkManager = $linkManager;
+    $this->adminContext = $adminContext;
+  }
+
+  /**
+   * Whether this breadcrumb builder should be used to build the breadcrumb.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
+   *
+   * @return bool
+   *   TRUE if this builder should be used or FALSE to let other builders
+   *   decide.
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    return $this->config->get('determine_menu') &&
+      !($this->config->get('disable_admin_page') &&
+        $this->adminContext->isAdminRoute($route_match->getRouteObject()));
+  }
+
+  /**
+   * Builds the breadcrumb.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
+   *
+   * @return \Drupal\Core\Breadcrumb\Breadcrumb
+   *   A breadcrumb.
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->addCacheContexts(['url.path']);
+
+    $menus = $this->config->get('menu_breadcrumb_menus');
+    uasort($menus, function ($a, $b) {
+      return SortArray::sortByWeightElement($a, $b);
+    });
+
+    $links = [];
+
+    foreach ($menus as $menu_name => $params) {
+      if (empty($params['enabled'])) {
+        continue;
+      }
+
+      // Get active trail for current route for given menu.
+      $trailIds = $this->menuActiveTrail->getActiveTrailIds($menu_name);
+      $trailIds = array_filter($trailIds);
+      // Skip if no links found.
+      if (empty($trailIds)) {
+        continue;
+      }
+
+      // Generate link for each menu item.
+      foreach (array_reverse($trailIds) as $id) {
+        $link = $this->linkManager->getInstance(['id' => $id]);
+        $text = $link->getTitle();
+        $url_object = $link->getUrlObject();
+        if ($url_object->getRouteName() != "<front>") {
+          $links[] = Link::fromTextAndUrl($text, $url_object);
+        }
+      }
+      break;
+    }
+
+    if (!count($links) && $this->config->get('hide_on_single_item')) {
+      return $breadcrumb;
+    }
+
+    if (!$this->config->get('remove_home')) {
+      $label = $this->config->get('home_as_site_name') ?
+        $this->configFactory->get('system.site')->get('name') :
+        t('Home');
+      $home = Link::createFromRoute($label, '<front>');
+      array_unshift($links, $home);
+    }
+
+    /** @var Link $current */
+    $current = array_pop($links);
+
+    if ($this->config->get('append_current_page')) {
+      if (!$this->config->get('current_page_as_link')) {
+        $current->setUrl(new Url('<none>'));
+      }
+
+      array_push($links, $current);
+    }
+
+    return $breadcrumb->setLinks($links);
+  }
+
+}
